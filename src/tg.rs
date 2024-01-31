aaf5969bd3550865e039d63e680dec47eba53de7
Matheus Carnelutt <carneloot@proton.me>
2024-01-31 03:59:44 -0300

    feat: stop escaping messages

--- src/tg.rs
+++ src/tg.rs
@@ -1,5 +1,3 @@
-use teloxide::utils::markdown::escape;
-
 pub enum TgResponse {
     Hello,
     IncorrectRequest,
@@ -7,22 +5,16 @@ pub enum TgResponse {
     FailedSetTimezone(String),
 }
 
-impl TgResponse {
-    pub fn to_unescaped_string(&self) -> String {
-        match self {
-            Self::Hello => concat!(
+impl From<TgResponse> for String {
+    fn from(value: TgResponse) -> Self {
+        match value {
+            TgResponse::Hello => concat!(
             "Hello! I'm Rações bot. My purpose is to track how much ration you have given your pet! \n\n",
             "Before we start, please either send me your location or manually select the timezone using the /settimezone command first."
             ).to_owned(),
-            Self::IncorrectRequest => "Incorrect request".to_owned(),
-            Self::ChosenTimezone(tz_name) => format!("Selected timezone: {tz_name}!"),
-            Self::FailedSetTimezone(tz_name) => format!("Failed to set timezone {}", tz_name),
+            TgResponse::IncorrectRequest => "Incorrect request".to_owned(),
+            TgResponse::ChosenTimezone(tz_name) => format!("Selected timezone: {}!", tz_name),
+            TgResponse::FailedSetTimezone(tz_name) => format!("Failed to set timezone {}", tz_name),
         }
-    }
-}
-
-impl From<TgResponse> for String {
-    fn from(value: TgResponse) -> Self {
-        escape(&value.to_unescaped_string())
     }
 }
